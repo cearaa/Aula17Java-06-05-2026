1c8987152e9536f80052e05424b694699956e181
RIkk <tarikmoussa90@gmail.com>
2026-05-06 10:57:30 -0300

    feat: classes e ident

--- src/br/com/fiap/bean/Funcionario.java
+++ src/br/com/fiap/bean/Funcionario.java
@@ -1,4 +1,15 @@
 package br.com.fiap.bean;
 
+import java.time.LocalDate;
+
 public class Funcionario {
+    //Atributos
+    private String nome;
+    private LocalDate dataDeNascimento;
+    private float valorHoraDeTrabalho;
+
+    //Construtor vazio
+    public Funcionario(){}
+
+    //Construtor com parametro
 }
