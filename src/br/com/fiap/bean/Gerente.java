949fcc72012fd53f827148e47e11e37a8965aa71
RIkk <tarikmoussa90@gmail.com>
2026-05-06 11:57:23 -0300

    feat: void constr

--- src/br/com/fiap/bean/Gerente.java
+++ src/br/com/fiap/bean/Gerente.java
@@ -1,5 +1,7 @@
 package br.com.fiap.bean;
 
+import java.time.LocalDate;
+
 public class Gerente extends Funcionario{
     //Atributos
     private float bonus;
@@ -8,4 +10,10 @@ public class Gerente extends Funcionario{
     public Gerente(){}
 
     //Construtor com parametro
+    public Gerente(String nome, LocalDate dataDeNascimento, float valorHoraDeTrabalho, float bonus) {
+        super(nome, dataDeNascimento, valorHoraDeTrabalho);
+        this.bonus = bonus;
+    }
+
+
 }
